b3f88be9f1fa03fe7c11431301bac72a1c7384d8
imradhe <radhesyam.musicals@gmail.com>
2023-12-27 12:50:19 +0530

    Updated migrations

--- views/migrate.php
+++ views/migrate.php
@@ -1,9 +1,12 @@
 <?php
+errors();
 // Define the SQL table names and their corresponding SQL files
         $tables = [
             'users' => 'auth/users.sql',
             'logs' => 'auth/logs.sql',
             'visitors' => 'auth/visitors.sql',
+            'weddings' => 'weddings/weddings.sql',
+            'wedding_images' => 'weddings/wedding_images.sql',
             // Add more tables as needed
         ];
         
