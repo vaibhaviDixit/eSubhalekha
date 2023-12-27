5d4ca8afbf82a4272d48fd7116f8aed7efe393f3
imradhe <radhesyam.musicals@gmail.com>
2023-12-27 11:12:22 +0530

    Updated Config

--- config_example.php
+++ config_example.php
@@ -8,18 +8,18 @@
  * SMTP configuration for email, SEO settings, and more.
  *
  * @package GraphenePHP
- * @version 1.0.0
+ * @version 2.0.0
  */
 
 $config = [
-    'APP_NAME' => 'GraphenePHP App',
-    'APP_TITLE' => 'GraphenePHP',
+    'APP_NAME' => 'eSubhalekha.com',
+    'APP_TITLE' => 'eSubhalekha.com - eInvites & Guest Management',
     'APP_URL' => 'http://localhost/',
-    'APP_SLUG' => 'GraphenePHP',
+    'APP_SLUG' => 'eSubhalekha.com',
     'DB_CONNECTION' => 'mysql',
     'DB_HOST' => 'localhost',
     'DB_PORT' => '3306',
-    'DB_DATABASE' => 'graphenephp',
+    'DB_DATABASE' => 'eSubhalekha',
     'DB_USERNAME' => 'root',
     'DB_PASSWORD' => '',
     'SMTP_DRIVER' => 'smtp',
@@ -29,14 +29,14 @@ $config = [
     'SMTP_PASSWORD' => '',
     'SMTP_ENCRYPTION' => 'tls',
     'OPENAI_API_KEY' => '',
-    'APP_DESC' => 'A Simple and light-weight PHP MVC Framework',
-    'APP_SHORT_TITLE' => 'GraphenePHP',
-    'APP_AUTHOR' => 'Radhe Shyam Salopanthula',
+    'APP_DESC' => 'personalized online invitation & Guest Management tool',
+    'APP_SHORT_TITLE' => 'eSubhalekha',
+    'APP_AUTHOR' => 'eSubhalekha.com',
     'APP_ICON' => 'assets/img/GraphenePHPIcon.png',
     'APP_OG_ICON' => 'assets/img/GraphenePHP.png',
     'APP_OG_ICON_MOBILE' => 'assets/img/GraphenePHP.png',
     'APP_THEME_COLOR' => '#FFFFFF',
-    'APP_KEYWORDS' => 'GraphenePHP App, Radhe Shyam Salopanthula',
+    'APP_KEYWORDS' => 'eSubhalekha.com, eSubhalekha, subhalekha, wedding, invitation, online invitation, Radhe Shyam Salopanthula',
     'APP_TWITTER_CREATOR' => '@imraadhe',
 ];
 
