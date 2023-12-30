d6b1076edf94ae7066945918dc7f8328ba37ce47
imradhe <radhesyam.musicals@gmail.com>
2023-12-30 19:26:03 +0530

    Updated

--- views/index.php
+++ views/index.php
@@ -2,15 +2,15 @@
 <html lang="en">
 <?php include("views/partials/head.php") ?>
 
-<body>
+<body class="bg-secondary">
 
     <?php require('views/partials/nav.php'); ?>
 
     <div id="app" class="container text-center">
         <img src="<?php assets("img/eSubhalekha.png"); ?>" alt="eSubhalekha.com logo" title="eSubhalekha.com logo"
             class="img-fluid mb-4">
-        <h1 class="fw-bold">eSubhalekha.com</b></h1>
-        <h4>Get Personalized Online Invitation Websites</h4>
+        <h1 class="text-primary fw-bold">eSubhalekha.com</b></h1>
+        <h4 class="text-black">Get Personalized Online Invitation Websites</h4>
         <a href="#" class="btn btn-primary btn-lg">Start Now</a>
             
     </div>
