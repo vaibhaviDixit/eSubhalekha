5ad80bbb73563a433a47038389dd62a06b8b3941
imradhe <radhesyam.musicals@gmail.com>
2023-12-31 02:35:26 +0530

    Modified users

--- models/auth/users.sql
+++ models/auth/users.sql
@@ -1,9 +1,12 @@
 -- users Table
 CREATE TABLE users (
-    email VARCHAR(255) PRIMARY KEY,
-    phone VARCHAR(10) NOT NULL,
+    userID VARCHAR(255) PRIMARY KEY NOT NULL, 
+    email VARCHAR(255) NOT NULL UNIQUE,
+    phone VARCHAR(10) NOT NULL UNIQUE,
     name VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL,
     role ENUM('admin', 'manager', 'partner', 'host', 'user') DEFAULT 'user',
-    status ENUM('verified', 'pending', 'deleted') DEFAULT 'pending'
+    status ENUM('verified', 'pending', 'deleted') DEFAULT 'pending',
+    createdAt DATETIME,
+    verifiedAt DATETIME
 );
