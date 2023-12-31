2841101bca97e59a402c9bc31cb86350f6fda202
imradhe <radhesyam.musicals@gmail.com>
2024-01-01 01:51:24 +0530

    Modified logs

--- models/auth/logs.sql
+++ models/auth/logs.sql
@@ -1,11 +1,11 @@
 -- logs Table
 CREATE TABLE logs (
     loginId VARCHAR(255) PRIMARY KEY,
-    email VARCHAR(255),
+    userID VARCHAR(255),
     loggedInAt DATETIME,
     ip VARCHAR(255),
     os VARCHAR(255),
     browser VARCHAR(255),
     loggedOutAt DATETIME,
-    FOREIGN KEY (email) REFERENCES users(email)
+    FOREIGN KEY (userID) REFERENCES users(userID)
 );
