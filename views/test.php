572edee125935425087c67302d53096a250ec4bd
vaibhaviDixit <vaibhavidixit511@gmail.com>
2024-01-01 14:28:13 +0530

    read update delete functions for Wedding controller tested

--- views/test.php
+++ views/test.php
@@ -1,25 +1,50 @@
 <?php
-errors(0);
+
+//display errors for debugging purpose
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 // Include the Wedding controller
 controller('Wedding');
 
 
 // Test data for creating a wedding
 $weddingData = [
-    'weddingID' => 'HariWedsSwathi',
-    'weddingName' => 'Pingali vari Vivaha mahotsavam',
+    'weddingID' => 'RahulWedsSakshi',
+    'weddingName' => 'Destination Vivaha Rajasthan',
     'fromRole' => 'bride',
-    'brideName' => 'Lakshmi', 
-    'groomName' => 'Raju', 
-    'host' => 'contact@imradhe.com',
+    'brideName' => 'Sakshi', 
+    'groomName' => 'Rahul', 
+    'host' => 'vaibhavidixit511@gmail.com',
 ];
 
 
 // Create an instance of the Wedding controller
 $weddingController = new Wedding();
 
-// Test the create function
-$result = $weddingController->create($weddingData);
+// // Test the create function
+// $result = $weddingController->create($weddingData);
+
+// // Display the result
+// echo json_encode($result);
+
+
+// // Test the update function
+// $resultOfUpdate = $weddingController->update('HarshWedsHarshada',$weddingData);
+
+// // Display the result
+// echo json_encode($resultOfUpdate);
+
+
+
+// Test the delete function
+$resultOfDelete = $weddingController->delete('RehanWedsHarshada');
 
 // Display the result
-echo json_encode($result);
+echo json_encode($resultOfDelete);
+
+
+
+
+
