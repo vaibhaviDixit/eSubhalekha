ed9ebd076fa5f39b893011a470438ecf8e80d98b
imradhe <radhesyam.musicals@gmail.com>
2024-01-02 14:13:12 +0530

    Modified

--- views/dashboard/wedding/new.php
+++ views/dashboard/wedding/new.php
@@ -3,10 +3,15 @@ locked(['user', 'host', 'manager', 'admin']);
 require('views/partials/dashboard/head.php');
 require('views/partials/dashboard/sidebar.php');
 
+
 DB::connect();
 $weddings = DB::select('weddings', '*', "lang = 'en'")->fetchAll();
+
+$languages = enumToArray(DB::select('information_schema.COLUMNS', 'COLUMN_TYPE', "TABLE_NAME = 'weddings' AND COLUMN_NAME = 'lang'", 'COLUMN_TYPE DESC')->fetch()[0]);
+
 DB::close();
 
+sort($languages);
 // current user id
 $userID=App::getUser()['email'];
 
@@ -21,19 +26,18 @@ $userID=App::getUser()['email'];
 
      <div>
      	
-     	<form>
-    	
+     	<form method="post" name="createWedding" class="form-wedding">
     	<div class="row">
     		<!-- Wedding ID -->
 		    <div class="mb-3 col-sm-6">
 		      <label for="weddingID" class="form-label">Wedding ID</label>
-		      <input type="text" class="form-control" id="weddingID" placeholder="Enter Wedding ID">
+		      <input type="text" class="form-control" id="weddingID" name="weddingID" placeholder="Enter Wedding ID">
 		    </div>
 
 		    <!-- Wedding Name -->
 		    <div class="mb-3 col-sm-6">
 		      <label for="weddingName" class="form-label">Wedding Name</label>
-		      <input type="text" class="form-control" id="weddingName" placeholder="Enter Wedding Name">
+		      <input type="text" class="form-control" id="weddingName" name="weddingName" placeholder="Enter Wedding Name">
 		    </div>
 
     	</div>
@@ -41,47 +45,24 @@ $userID=App::getUser()['email'];
    	<div class="row">
    		 <!-- From (Bride/Groom) -->
 		    <div class="mb-3 col-sm-6">
-		      <label class="form-label">From</label>
-		      <div class="d-flex gap-2">
-		      	<div class="form-check">
-		        <input class="form-check-input" type="radio" name="fromRadio" id="brideRadio" value="bride">
-		        <label class="form-check-label" for="brideRadio">
-		          Bride
-		        </label>
-		      	</div>
-
-		      <div class="form-check">
-		        <input class="form-check-input" type="radio" name="fromRadio" id="groomRadio" value="groom">
-		        <label class="form-check-label" for="groomRadio">
-		          Groom
-		        </label>
-		      </div>
-
-		      </div>
-		    
+		      <label class="form-label" for="fromSelect">From</label>
+			  
+		      <select class="form-select" id="fromSelect" name="fromSelect">
+			    <option value="bride">Bride</option>
+			    <option value="groom">Groom</option>
+		      
+		      </select>
 		    </div>
 
 		    <!-- Language -->
 		    <div class="mb-3 col-sm-6">
 		      <label for="languageSelect" class="form-label">Language</label>
-		      <select class="form-select" id="languageSelect">
-		        <option selected>Select Language</option>
-
-			    <option value="en">English</option>
-			    <option value="as">Assamese</option>
-			    <option value="bn">Bengali</option>
-			    <option value="gu">Gujarati</option>
-			    <option value="hi">Hindi</option>
-			    <option value="kn">Kannada</option>
-			    <option value="ml">Malayalam</option>
-			    <option value="mr">Marathi</option>
-			    <option value="ne">Nepali</option>
-			    <option value="or">Odia</option>
-			    <option value="pa">Punjabi</option>
-			    <option value="ta">Tamil</option>
-			    <option value="te">Telugu</option>
-			    <option value="ur">Urdu</option>
-		      
+		      <select class="form-select" id="languageSelect" name="languageSelect">
+			    <?php foreach ($languages as $lang) {
+					?>
+					<option value="<?=$lang?>" <?php if($lang == 'en') echo 'selected' ?>><?=Locale::getDisplayLanguage ($lang, 'en')?></option>	
+					<?php
+				}		   ?>   
 		      </select>
 		    </div>
 
@@ -91,19 +72,19 @@ $userID=App::getUser()['email'];
     	<!-- Bride Name -->
 	    <div class="mb-3 col-sm-6">
 	      <label for="brideName" class="form-label">Bride Name</label>
-	      <input type="text" class="form-control" id="brideName" placeholder="Enter Bride Name">
+	      <input type="text" class="form-control" id="brideName" name="brideName" placeholder="Enter Bride Name">
 	    </div>
 
 	    <!-- Groom Name -->
 	    <div class="mb-3 col-sm-6">
 	      <label for="groomName" class="form-label">Groom Name</label>
-	      <input type="text" class="form-control" id="groomName" placeholder="Enter Groom Name">
+	      <input type="text" class="form-control" id="groomName" name="groomName" placeholder="Enter Groom Name">
 	    </div>
 
     </div>
 
     <!-- Submit Button -->
-    <button type="submit" class="btn btn-primary">Submit</button>
+    <button type="submit" name="btn-submit" class="btn btn-primary">Create Wedding</button>
   </form>
 
      </div>
