a22db8f22a1205274ac7e58c264291a728800efb
vaibhaviDixit <vaibhavidixit511@gmail.com>
2024-01-21 09:11:17 +0530

    gallery add btn fix,sidebar sequence

--- views/dashboard/wedding/timeline.php
+++ views/dashboard/wedding/timeline.php
@@ -1,4 +1,5 @@
 <?php
+
 locked(['user', 'host', 'manager', 'admin']);
 require('views/partials/dashboard/head.php');
 require('views/partials/dashboard/sidebar.php');
@@ -10,8 +11,63 @@ DB::close();
 controller("Wedding");
 $wedding = new Wedding();
 $weddingData = $wedding->getWedding($_REQUEST['id'], $_REQUEST['lang']);
+
+$required="required";
+
+if($weddingData['timeline']){
+    $required="";
+}
+
 $timeline = [];
 $timeline = json_decode($weddingData['timeline'], true);
+
+controller("Gallery");
+$gallery = new Gallery();
+
+
+
+function getImgURL($name){
+    $gallery = new Gallery();
+    $row=$gallery->getGalleryImg($_REQUEST['id'],$name);
+    
+    if($row['imageURL']){
+        return $row['imageURL'];
+    }
+    else{
+        return false;
+    }
+    
+}
+ if(isset($_REQUEST['delTimeline'])){
+
+    controller("AWSBucket");
+    $awsObj=new AWSBucket();
+
+        $eventid=$_REQUEST['delTimeline'];
+        $imgurl=getImgURL($timeline[$eventid]['event']);
+
+        unset($timeline[$eventid]);
+
+        $_REQUEST['timeline'] = $timeline;
+        $createWedding = $wedding->update($_REQUEST['id'], $_REQUEST['lang'], $_REQUEST);
+
+         if (!$createWedding['error']) {
+                $gallery=new Gallery();
+                $getrow=$gallery->deleteByURL($_REQUEST['id'],$imgurl);
+                
+                if(!$getrow['error']){
+                    $awsObj=new AWSBucket();
+                    $awsObj->deleteFromAWS($imgurl);
+
+                    echo "<script>alert('Deleted Successfully'); window.history.back(); window.location.reload(true); </script>";
+                }
+         }
+        else{
+            echo "<script>alert('Failed to delete');window.history.back(); window.location.reload(true);  </script>";
+        }
+
+    }
+
 ?>
 
 <head>
@@ -22,28 +78,67 @@ $timeline = json_decode($weddingData['timeline'], true);
     <h1 class="h2">Events</h1>
 
 
-    <form id="form" method="post">
+    <form id="form" method="post" enctype="multipart/form-data">
         <?php
 
         if (isset($_POST['btn-submit'])) {
 
+            controller("AWSBucket");
+            $awsObj=new AWSBucket();
+
             $timeline = array();
+            $eventImgArray=array();
 
             if ($_REQUEST['event'] != null) {
+
                 for ($i = 0; $i < count($_REQUEST['event']); $i++) {
-                    $timeline[$i] = [
-                        'event' => $_REQUEST['event'][$i],
-                        'startTime' => $_REQUEST['startTime'][$i],
-                        'endTime' => $_REQUEST['endTime'][$i],
-                        'locationURL' => $_REQUEST['locationURL'][$i],
-                        'venue' => $_REQUEST['venue'][$i],
-                        'address' => str_replace("\r\n", "<br>", $_REQUEST['address'][$i])
-                    ];
-                }
+
+                    if(isset($_FILES['eventPic']['name'][$i]) && strlen($_FILES['eventPic']['name'][$i])>0){
+
+                        $filearr=array();
+
+                        $filearr['eventPic']['name']=$_FILES['eventPic']['name'][$i];
+                        $filearr['eventPic']['tmp_name']=$_FILES['eventPic']['tmp_name'][$i];
+
+                        $uploadedURL = $awsObj->uploadToAWS($filearr,'eventPic');
+
+                        if($uploadedURL['error']){
+                            echo '<div class="alert alert-danger">'.$uploadedURL['errorMsg'].'</div>';
+                        }
+
+                        $eventImgArray[$i]=[
+                            'weddingID'=>$_REQUEST['id'],
+                            'imageURL' => $uploadedURL['url'],
+                            'imageName'=> $_REQUEST['event'][$i],
+                            'type'=>'event'
+                        ];
+                    }
+
+                    $awsObj->deleteFromAWS(getImgURL($_REQUEST['event'][$i]));
+                        
+                        $timeline[$i] = [
+                            'event' => $_REQUEST['event'][$i],
+                            'startTime' => $_REQUEST['startTime'][$i],
+                            'endTime' => $_REQUEST['endTime'][$i],
+                            'locationURL' => $_REQUEST['locationURL'][$i],
+                            'venue' => $_REQUEST['venue'][$i],
+                            'address' => str_replace("\r\n", "<br>", $_REQUEST['address'][$i])
+                        ];
+                    
+                }   
+                
             }
+
+
             $_REQUEST['timeline'] = $timeline;
 
             $createWedding = $wedding->update($_REQUEST['id'], $_REQUEST['lang'], $_REQUEST);
+
+            $addToGalleryEvents=array();
+
+            foreach ($eventImgArray as $key => $value) {
+                $addToGalleryEvents = $gallery->update($eventImgArray[$key]);
+            }
 
             if ($createWedding['error']) {
                 ?>
@@ -56,13 +151,27 @@ $timeline = json_decode($weddingData['timeline'], true);
                     ?>
                 </div>
                 <?php
-            } else
+            }
+            else if ($addToGalleryEvents['error']) {
+                ?>
+                <div class="alert alert-danger">
+                    <?php
+                    foreach ($addToGalleryEvents['errorMsgs'] as $msg) {
+                        if (count($msg))
+                            echo $msg[0] . "<br>";
+                    }
+                    ?>
+                </div>
+                <?php
+            }
+             else
                 redirect("wedding/" . $_REQUEST['id'] . "/" . $_REQUEST['lang'] . "/additional-details");
 
         }
 
 
         ?>
+
         <div class="row">
             <div class="col-md-12">
                 <button type="button" class="btn btn-primary mb-3 float-end" id="addRowBtn"><i
@@ -81,8 +190,10 @@ $timeline = json_decode($weddingData['timeline'], true);
                                 ?>
                                 <tr id="row<?= $i ?>" class="row">
                                     <td>
-                                        <button class="btn btn-danger float-end" onclick="deleteRow(<?= $i ?>)"><i
-                                                class="bi bi-trash-fill"></i></button>
+                                    
+                                    <a href="?delTimeline=<?= $i ?>">
+                                        <button class="btn btn-danger float-end"><i class="bi bi-trash-fill"></i></button>
+                                    </a>
 
                                         <div class="card mb-3 p-3">
                                             <div class="row">
@@ -90,7 +201,7 @@ $timeline = json_decode($weddingData['timeline'], true);
                                                 <div class="mb-2 col-sm-6 col-md-4 col-lg-3">
                                                     <label for="event" class="form-label">Event Name</label>
                                                     <input type="text" class="form-control"
-                                                        placeholder="Enter Bride's Father Name"
+                                                        placeholder="Enter Event Name"
                                                         value="<?= $timeline[$i]['event'] ?>" name="event[]">
                                                     <strong id="eventMsg" class="text-danger errorMsg my-2 fw-bolder"></strong>
                                                 </div>
@@ -132,6 +243,18 @@ $timeline = json_decode($weddingData['timeline'], true);
                                                     <strong id="AddressMsg"
                                                         class="text-danger errorMsg my-2 fw-bolder"></strong>
                                                 </div>
+
+                                                <div class="mb-2 col-sm-6 col-md-4 col-lg-6">
+                                                      <label for="eventPic<?php echo $i; ?>" class="form-label" style="position: relative;">
+                                                           Event Image <br>
+                                                            <img id="eventImg<?php echo $i; ?>" src="<?php echo getImgURL($timeline[$i]['event']); ?>" alt="Event Image" class="rounded-circle" style="width: 150px; height: 150px;">
+
+                                                            </label>
+
+                                                            <input type="file" class="form-control" id="eventPic<?php echo $i; ?>" accept="image/*" name="eventPic[]" <?= $required ?>  onchange="displayEventImage(this,<?php echo $i; ?>)">
+                                                 </div>
+
+
                                             </div>
                                         </div>
                                     </td>
@@ -154,6 +277,25 @@ $timeline = json_decode($weddingData['timeline'], true);
 </main>
 
 <script>
+
+    // display event img
+            function displayEventImage(input,rowNum) {
+
+              const file = input.files[0];
+
+              if (file) {
+                const reader = new FileReader();
+
+                reader.onload = function (e) {
+                  document.getElementById('eventImg'+rowNum).src =e.target.result;
+
+                };
+
+                reader.readAsDataURL(file);
+              }
+
+            }
+
     // JavaScript code for dynamic form functionality
     $(document).ready(function () {
         // Counter to keep track of the number of rows
@@ -180,6 +322,7 @@ $timeline = json_decode($weddingData['timeline'], true);
               <input type="text" class="form-control" name="event[]">
               <strong id="eventMsg" class="text-danger errorMsg my-2 fw-bolder"></strong>
             </div>
+
                 
             <div class="mb-2 col-sm-6 col-md-4 col-lg-3">
               <label for="startTime" class="form-label">Event Start Time</label>
@@ -211,6 +354,18 @@ $timeline = json_decode($weddingData['timeline'], true);
               <textarea class="form-control" name="address[]" rows="3"></textarea>
               <strong id="AddressMsg" class="text-danger errorMsg my-2 fw-bolder"></strong>
             </div>
+
+            <div class="mb-2 col-sm-6 col-md-4 col-lg-6">
+              <label for="eventPic${rowCount}" class="form-label" style="position: relative;">
+                   Event Image <br>
+                    <img id="eventImg${rowCount}" src="<?php assets('img/upload.png'); ?>" alt="Event Image" class="rounded-circle" style="width: 150px; height: 150px;">
+
+                    </label>
+
+                    <input type="file" class="form-control" id="eventPic${rowCount}" accept="image/*" name="eventPic[]" required  onchange="displayEventImage(this,${rowCount})">
+            </div>
+
+
             </div>
            </div>
                 </td>
@@ -226,6 +381,8 @@ $timeline = json_decode($weddingData['timeline'], true);
             // Remove the row with the specified ID
             $("#row" + rowId).remove();
         };
+
+        
     });
 
 </script>
