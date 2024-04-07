802e8b1bd2ac5a3a0fd57011ab0e0aa6678491fb
vaibhaviDixit <vaibhavidixit511@gmail.com>
2024-04-07 23:29:48 +0530

    login issue resolved

---
+++ views/migrate.php
@@ -0,0 +1,19 @@
+<?php
+errors();
+// Define the SQL table names and their corresponding SQL files
+        $tables = [
+            'users' => 'auth/users.sql',
+            'logs' => 'auth/logs.sql',
+            'verification' => 'auth/verification.sql',
+            'visitors' => 'auth/visitors.sql',
+            'weddings' => 'weddings/weddings.sql',
+            'payments' => 'weddings/payments.sql',
+            'guests' => 'weddings/guests.sql',
+            'gallery' => 'weddings/gallery.sql',
+            // Add more tables as needed
+        ];
+        
+        // Migrate tables
+        $migrate = Migrator::migrate($tables);
+        
+        echo json_encode($migrate);
