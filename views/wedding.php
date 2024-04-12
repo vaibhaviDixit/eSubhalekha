b9f742d8dd307f14ff5b2a48017c59970b2cbefd
imradhe <radhesyam.musicals@gmail.com>
2024-04-12 15:37:11 +0530

    Implemented ThemeController

--- views/wedding.php
+++ views/wedding.php
@@ -1,3 +1,9 @@
 <?php
 
-echo "wedding.php";
+controller('Theme');
+
+// Instantiate the ThemeController class
+$themeController = new ThemeController();
+
+// Call the getThemes method
+$themes = $themeController->render('sample_theme', $_REQUEST['type']);
