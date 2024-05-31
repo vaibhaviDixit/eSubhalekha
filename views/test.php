08d93301af44f1cdd8c7622ead843ec4e9f70bbf
vaibhaviDixit <vaibhavidixit511@gmail.com>
2024-05-31 11:20:18 +0530

    brevosms issue

--- views/test.php
+++ views/test.php
@@ -1,9 +1,26 @@
 <?php
 
-controller('Theme');
+errors(1);
+
+// controller('Theme');
 
 // Instantiate the ThemeController class
-$themeController = new ThemeController();
+// $themeController = new ThemeController();
 
 // Call the getThemes method
-$themes = $themeController->render('fairytale_theme', $_REQUEST['type']);
+// $themes = $themeController->render('fairytale_theme', $_REQUEST['type']);
+
+controller('BrevoSMS');
+
+// API: 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
+
+
+// Usage
+$brevoSms = new BrevoSMS();
+$sender = 'Vaibhavi';
+$recipient = '+918767431102';
+$message = 'Hello, this is a test message!';
+$webUrl = '';
+$brevoSms->sendTransactionalSms($sender, $recipient, $message);
+
+
